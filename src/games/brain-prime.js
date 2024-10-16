33e95fb912af7959f32d18987e4c22b1fd88b296
rita sh <lapandh@gmail.com>
2024-10-16 23:41:03 +0300

    refactoring 2

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -3,9 +3,9 @@
 import { playGame } from '../index.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-let correctAnswer;
 
 const isPrime = (num) => {
+  let correctAnswer;
   if (num === 1) {
     correctAnswer = 'yes';
   } else {
