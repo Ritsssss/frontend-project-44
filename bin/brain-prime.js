1b6b154181a6274c5f946137a363e7081c3cf995
rita sh <lapandh@gmail.com>
2024-11-07 18:14:49 -0500

    linter 4

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import playGame from '../src/index';
-import brainPrime from '../src/games/brain-prime';
+import playGame from '../src/index.js';
+import brainPrime from '../src/games/brain-prime.js';
 
 const { description, gameLogic } = brainPrime();
 playGame(description, gameLogic);
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import playGame from '../src/index';
-import brainProgression from '../src/games/brain-progression';
+import playGame from '../src/index.js';
+import brainProgression from '../src/games/brain-progression.js';
 
 const { description, gameLogic } = brainProgression();
 playGame(description, gameLogic);
