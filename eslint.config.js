58e61dcdadafb6c983d979c90adefc2f340e2424
rita sh <lapandh@gmail.com>
2024-11-08 15:16:46 -0500

    eslinter updates

--- eslint.config.js
+++ eslint.config.js
@@ -16,4 +16,24 @@ export default [
   { ignores: ['eslint.config.js'] },
   { languageOptions: { globals: globals.browser } },
   ...compat.extends('airbnb'),
+  {
+    rules: {
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['__filename', '__dirname'],
+        },
+      ],
+      'import/extensions': [
+        'error',
+        {
+          js: 'always',
+        },
+      ],
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off',
+    },
+  },
 ];
